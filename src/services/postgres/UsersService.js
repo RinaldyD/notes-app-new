d34b62d06865183f9b57d9b3367e56ef7c073f35
RinaldyD <darmawan.rinaldy@gmail.com>
2023-06-19 23:05:38 +0700

    authorization implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -54,7 +54,7 @@ class UsersService {
 
     if (result.rows.length > 0) {
       throw new InvariantError(
-        'Gagal menambahkan user. Username sudah digunakan.'
+        'Gagal menambahkan user. Username sudah digunakan.',
       );
     }
   }
